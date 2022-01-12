be77dbe15d63c1f4c6fabf3e0c96366f023d5d39
otenim <snake_istobelieve@keio.jp>
2022-01-13 03:44:49 +0900

    update

--- main.cpp
+++ main.cpp
@@ -7,6 +7,18 @@
 
 int main()
 {
-    printf("Hello, Mbed!\n");
+    AnalogIn adc_vref(PinName::ADC_VREF);
+
+    // Read VREFINT_CAL (always takes a constant value)
+    uint16_t vrefint_cal = *((uint16_t *)VREFINT_CAL_ADDR);
+    while (true) {
+        // Read VREFINT (the 12-bit output is left-aligned)
+        uint16_t vrefint = adc_vref.read_u16() >> 4;
+        // Calc VDDA
+        float vdda = (VREFINT_CAL_VREF / 1000.0f) * vrefint_cal / vrefint;
+        printf("VDDA: %f [v]\n", vdda);
+        // Sleep for 10 [s].
+        ThisThread::sleep_for(10s);
+    }
     return 0;
 }
