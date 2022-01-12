69ccce7c658146cd74e89196079cf39419088531
otenim <snake_istobelieve@keio.jp>
2022-01-12 20:17:11 +0900

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2022 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "mbed.h"
+
+
+int main()
+{
+    printf("Hello, Mbed!\n");
+    return 0;
+}
