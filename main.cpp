853df99b4420dcb45855bcdbc99bb57bf216dbdc
otenim <snake_istobelieve@keio.jp>
2022-01-13 09:01:26 +0900

    update

--- main.cpp
+++ main.cpp
@@ -11,12 +11,14 @@ int main()
 
     // Read VREFINT_CAL (always takes a constant value)
     uint16_t vrefint_cal = *((uint16_t *)VREFINT_CAL_ADDR);
+    // Calc VREFINT_CHARAC (VREFINT_CAL_VREF is the mV version of VREFINT_CHARAC)
+    float vrefint_charac = VREFINT_CAL_VREF / 1000.0f;
     while (true) {
         // Read VREFINT (the 12-bit output is left-aligned)
         uint16_t vrefint = adc_vref.read_u16() >> 4;
-        // Calc VDDA
-        float vdda = (VREFINT_CAL_VREF / 1000.0f) * vrefint_cal / vrefint;
-        printf("VDDA: %f [v]\n", vdda);
+        // Calc VDD
+        float vdd = vrefint_charac * vrefint_cal / vrefint;
+        printf("VDD: %f [v]\n", vdd);
         // Sleep for 10 [s].
         ThisThread::sleep_for(10s);
     }
